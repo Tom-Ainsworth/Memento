51bb36480b8a8002758a624e68b39843f8c24612
Tom Ainsworth <tomainsworth94@gmail.com>
2022-08-20 21:14:20 +0100

    Create shuffle utility to create the array of assets to display. Call card in App.js, and create card and header components

--- memento/src/App.js
+++ memento/src/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import shuffle from './utilities/shuffle';
 
 function App() {
-	return <div className="grid"></div>;
+	const [cards, setCards] = useState([shuffle]); // Cards array from assets
+
+	return (
+		<div className="grid">
+			{cards.map((card) => {
+				const { image, id, matched } = card;
+
+				return (
+					<Card
+						key={id}
+						image={image}
+						selected={false}
+						onClick={() => {}}
+					/>
+				);
+			})}
+		</div>
+	);
 }
 
 export default App;
